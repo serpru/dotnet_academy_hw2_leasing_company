4dbda55b8567ce865b633e69ce9eb517643f57f1
serpru <saviodrg@outlook.com>
2024-01-03 18:07:12 +0100

    Add project files.

---
+++ dotnet_academy_hw2_leasing_company/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace dotnet_academy_hw2_leasing_company
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
